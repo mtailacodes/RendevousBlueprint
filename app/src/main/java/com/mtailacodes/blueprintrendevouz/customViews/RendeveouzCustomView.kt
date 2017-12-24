9afb0a6b48e9c9218b77b210241a937ba61ee75a
MTailaCodes <mtailacodes@gmail.com>
2017-12-23 19:36:31 -0500

    began working on custom textInputLayout

--- app/src/main/java/com/mtailacodes/blueprintrendevouz/customViews/RendeveouzCustomView.kt
+++ app/src/main/java/com/mtailacodes/blueprintrendevouz/customViews/RendeveouzCustomView.kt
@@ -1,5 +1,21 @@
 package com.mtailacodes.blueprintrendevouz.customViews
 
+import android.content.Context
+import android.support.constraint.ConstraintLayout
+import android.util.AttributeSet
+import java.util.jar.Attributes
+
 /**
  * Created by matthewtaila on 12/23/17.
  */
+
+class RendeveouzCustomView : ConstraintLayout {
+
+    constructor(context: Context) : this(context, null)
+    constructor(context: Context, attrs: AttributeSet?) : this(context, attrs, 0)
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
+
+    }
+
+
+}
