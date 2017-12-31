4308a3564a4b9ebefe42df4bf87a884dd65d59e2
MTailaCodes <mtailacodes@gmail.com>
2017-12-30 22:26:37 -0500

    updated Rendevouz model to store pushID member

--- app/src/main/java/com/mtailacodes/blueprintrendevouz/models/user/user/login/RendevouzUserModel.kt
+++ app/src/main/java/com/mtailacodes/blueprintrendevouz/models/user/user/login/RendevouzUserModel.kt
@@ -1,22 +1,48 @@
 package com.mtailacodes.blueprintrendevouz.models.user.user.login
 
+import android.os.Parcel
+import android.os.Parcelable
 import com.google.firebase.auth.FirebaseUser
 
 /**
  * Created by matthewtaila on 12/24/17.
  */
 
-class RendevouzUserModel {
+class RendevouzUserModel() : Parcelable {
 
     internal var  emailAddress  = "defaulUser"
-    internal var  password = "defaulUser"
     internal var  UuID = "defaulUser"
-    internal lateinit var fireBaseUser : FirebaseUser
+    internal var pushID = "defaultUser"
+    internal var username = "defaultUser"
+
+    constructor(parcel: Parcel) : this() {
+        emailAddress = parcel.readString()
+        UuID = parcel.readString()
+        pushID = parcel.readString()
+        username = parcel.readString()
+    }
 
 
-    constructor()
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(emailAddress)
+        parcel.writeString(UuID)
+        parcel.writeString(pushID)
+        parcel.writeString(username)
+    }
 
+    override fun describeContents(): Int {
+        return 0
+    }
 
+    companion object CREATOR : Parcelable.Creator<RendevouzUserModel> {
+        override fun createFromParcel(parcel: Parcel): RendevouzUserModel {
+            return RendevouzUserModel(parcel)
+        }
+
+        override fun newArray(size: Int): Array<RendevouzUserModel?> {
+            return arrayOfNulls(size)
+        }
+    }
 
 
 }
