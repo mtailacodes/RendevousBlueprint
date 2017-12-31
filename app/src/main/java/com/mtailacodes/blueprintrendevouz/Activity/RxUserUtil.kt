64376a7244cb6c7440ecf5e3d095e34590cbbb9c
MTailaCodes <mtailacodes@gmail.com>
2017-12-30 22:27:25 -0500

    Created and implemented util and constants classes

---
+++ app/src/main/java/com/mtailacodes/blueprintrendevouz/Activity/RxUserUtil.kt
@@ -0,0 +1,23 @@
+package com.mtailacodes.blueprintrendevouz.Activity
+
+import com.google.firebase.auth.FirebaseAuth
+import rx.Single
+
+class RxUserUtil {
+
+    val firebaseAuth = FirebaseAuth.getInstance()
+
+    fun createUserWithEmailAndPassword(email: String, password: String): Single<Unit> {
+        return Single.create<Unit> { singleSubscriber ->
+            firebaseAuth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            singleSubscriber.onSuccess(null)
+                        } else {
+                            singleSubscriber.onError(task.exception)
+                        }
+                    }
+        }
+    }
+
+}
---
+++ app/src/main/java/com/mtailacodes/blueprintrendevouz/Util/Constants.kt
@@ -0,0 +1,11 @@
+package com.mtailacodes.blueprintrendevouz.Util
+
+/**
+ * Created by matthewtaila on 12/30/17.
+ */
+
+object Constants {
+
+    open var RENDEVOUZ_USER_MODEL_BUNDLE = "Rendevouz user model bundle"
+
+}
