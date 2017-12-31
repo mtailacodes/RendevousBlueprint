17ea5b7e84865bbdfcd99fe9360542b86bc11277
MTailaCodes <mtailacodes@gmail.com>
2017-12-31 00:58:25 -0500

    no message

--- app/src/main/java/com/mtailacodes/blueprintrendevouz/Util/Constants.kt
+++ app/src/main/java/com/mtailacodes/blueprintrendevouz/Util/Constants.kt
@@ -6,6 +6,7 @@ package com.mtailacodes.blueprintrendevouz.Util
 
 object Constants {
 
+    open var GOOGLE_MAPS_API_KEY = "AIzaSyDL6gX0mckMFx3cBXMJqfnyRGe56g0DuSc"
     open var RENDEVOUZ_USER_MODEL_BUNDLE = "Rendevouz user model bundle"
 
 }
